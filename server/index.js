45e9f6f8afe1f40800aa4021c170d1c0bf89243d
Thomas Walsh <82677648+thomaswalsh92@users.noreply.github.com>
2022-01-31 12:10:35 +0000

    Testing heroku without oAuth scopes

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ dotenv.config()
 let spotify_client_id = process.env.SPOTIFY_CLIENT_ID
 let spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
 
-//let spotify_redirect_uri = 'https://songmachine.herokuapp.com/auth/callback'
-let spotify_redirect_uri = 'http://localhost:5000/auth/callback'
+let spotify_redirect_uri = 'https://songmachine.herokuapp.com/auth/callback'
+//let spotify_redirect_uri = 'http://localhost:5000/auth/callback'
 
 let generateRandomString = function (length) {
   var text = '';
