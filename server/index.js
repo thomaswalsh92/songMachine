5556c275330b1edfbff2da42e80fb34746b14bb5
Thomas Walsh <82677648+thomaswalsh92@users.noreply.github.com>
2022-02-01 11:27:30 +0000

    Bringing back to working build, test merge into main

--- server/index.js
+++ server/index.js
@@ -133,11 +133,11 @@ app.get('/api/recommendations', async (req, res) => {
   })
 });
 
-// app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
-// app.get('/', (req, res) =>  {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', (req, res) =>  {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
